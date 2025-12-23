05a010d380b19b216cba85593075b9792c920772
Liviu Popa <liviucst@gmail.com>
2025-12-23 16:02:40 +0200

    Created Mapping module

---
+++ KernelModules/Mapping/source/mapping_main.c
@@ -0,0 +1,152 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/slab.h>
+#include <linux/sysfs.h>
+
+#define SUCCESS 0
+#define MAX_COMMAND_STR_LENGTH 32
+#define MAX_STATUS_STR_LENGTH 16
+#define MAX_KEY_STR_LENGTH 64
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("This module illustrates a dictionary.");
+MODULE_AUTHOR("Liviu Popa");
+
+/* VARIABLES AND PARAMETERS */
+
+struct mapping_data
+{
+    struct kobject mapping_kobj;
+    char key[MAX_KEY_STR_LENGTH];
+    int value;
+    char command[MAX_COMMAND_STR_LENGTH];
+    char status[MAX_STATUS_STR_LENGTH];
+};
+
+struct mapping_data* data = NULL;
+
+/* SYSFS access methods for attributes */
+
+static ssize_t key_show(struct kobject* kobj, struct kobj_attribute* attr, char* buf)
+{
+    struct mapping_data* data = container_of(kobj, struct mapping_data, mapping_kobj);
+    return sysfs_emit(buf, "%s\n", data->key);
+}
+
+static ssize_t key_store(struct kobject* kobj, struct kobj_attribute* attr, const char* buf, size_t count)
+{
+    struct mapping_data* data = container_of(kobj, struct mapping_data, mapping_kobj);
+    // memset(data->command, '\0', MAX_KEY_STR_LENGTH);
+    // strncpy(data->command, buf, MAX_KEY_STR_LENGTH - 1);
+
+    return count;
+}
+
+static ssize_t value_show(struct kobject* kobj, struct kobj_attribute* attr, char* buf)
+{
+    struct mapping_data* data = container_of(kobj, struct mapping_data, mapping_kobj);
+    return sysfs_emit(buf, "%d\n", data->value);
+}
+
+static ssize_t value_store(struct kobject* kobj, struct kobj_attribute* attr, const char* buf, size_t count)
+{
+    struct mapping_data* data = container_of(kobj, struct mapping_data, mapping_kobj);
+    int result = kstrtoint(buf, 10, &data->value);
+    return result < 0 ? 0 : count;
+}
+
+// no show to be defined here as the command is write-only
+static ssize_t command_store(struct kobject* kobj, struct kobj_attribute* attr, const char* buf, size_t count)
+{
+    struct mapping_data* data = container_of(kobj, struct mapping_data, mapping_kobj);
+
+    // store_command(data, buf);
+    return count;
+}
+
+// no store to be defined here as the status is read-only
+static ssize_t status_show(struct kobject* kobj, struct kobj_attribute* attr, char* buf)
+{
+    struct mapping_data* data = container_of(kobj, struct mapping_data, mapping_kobj);
+    return sysfs_emit(buf, "%s\n", data->status);
+}
+
+static void mapping_release(struct kobject* kobj)
+{
+    struct mapping_data* data = container_of(kobj, struct mapping_data, mapping_kobj);
+    pr_info("%s: freeing data object that contains kobject \"%s\"\n", THIS_MODULE->name, kobj->name);
+    kfree(data);
+}
+
+/* SYSFS attributes */
+
+static struct kobj_attribute key_attribute = __ATTR(key, 0600, key_show, key_store);
+static struct kobj_attribute value_attribute = __ATTR(value, 0600, value_show, value_store);
+static struct kobj_attribute command_attribute = __ATTR(command, 0200, NULL, command_store);
+static struct kobj_attribute status_attribute = __ATTR(status, 0400, status_show, NULL);
+
+static struct attribute* mapping_attrs[] = {&key_attribute.attr, &value_attribute.attr, &command_attribute.attr,
+                                            &status_attribute.attr, NULL};
+
+// these statements can be replaced by: "ATTRIBUTE_GROUPS(mapping);" (see sysfs.h)
+// I preferred to keep the unwrapped for better readability
+static const struct attribute_group mapping_group = {.attrs = mapping_attrs};
+static const struct attribute_group* mapping_groups[] = {&mapping_group, NULL};
+
+// kobj_sysfs_ops is the default sysfs_ops (binds all access functions defined above into mapping_ktype)
+static struct kobj_type mapping_ktype = {
+    .sysfs_ops = &kobj_sysfs_ops, .release = mapping_release, .default_groups = mapping_groups};
+
+/* INIT/EXIT */
+
+static int mapping_init(void)
+{
+    // resulting directory structure: "/sys/kernel/mapping"
+    // - kernel_kobj (parent kobject) => "/sys/kernel"
+    // - mapping_kobj_name => "/mapping"
+    // - all attributes appearing as files in "/mapping"
+    const char* mapping_kobj_name = "mapping";
+
+    int result = SUCCESS;
+
+    pr_info("%s: initializing module\n", THIS_MODULE->name);
+    data = kzalloc(sizeof(struct mapping_data), GFP_KERNEL);
+
+    if (data)
+    {
+        result = kobject_init_and_add(&data->mapping_kobj, &mapping_ktype, kernel_kobj, "%s", mapping_kobj_name);
+        if (result != SUCCESS)
+        {
+            result = -ENOMEM;
+        }
+    }
+    else
+    {
+        result = -ENOMEM;
+    }
+
+    if (result == SUCCESS)
+    {
+        kobject_uevent(&data->mapping_kobj, KOBJ_ADD);
+    }
+    else if (result == -ENOMEM)
+    {
+        pr_err("%s: unable to initialize sysfs object \"%s\" (no memory)!\n", THIS_MODULE->name, mapping_kobj_name);
+    }
+
+    return result;
+}
+
+static void mapping_exit(void)
+{
+    pr_info("%s: putting the kobject: %s\n", THIS_MODULE->name, data->mapping_kobj.name);
+
+    // this calls the release function (mapping_release()) for the data object containing the kobject
+    kobject_put(&data->mapping_kobj);
+
+    pr_info("%s: the module exited!\n", THIS_MODULE->name);
+}
+
+module_init(mapping_init);
+module_exit(mapping_exit);
