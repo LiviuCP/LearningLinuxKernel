0715df19ad70c3a966edb38cb4bb75f127492337
Liviu Popa <liviucst@gmail.com>
2026-01-02 22:56:47 +0200

    Updated mapping_impl (made some functions static)

--- KernelModules/Mapping/include/mapping_impl.h
+++ KernelModules/Mapping/include/mapping_impl.h
@@ -8,16 +8,6 @@
 #define MAX_KEY_STR_LENGTH 64
 #define MAX_ELEMENTS_COUNT 256
 
-static const char* update_command = "update";
-static const char* remove_command = "remove";
-static const char* get_command = "get";
-static const char* reset_command = "reset";
-
-static const size_t update_command_length = 6;
-static const size_t remove_command_length = 6;
-static const size_t get_command_length = 3;
-static const size_t reset_command_length = 5;
-
 struct mapping_data
 {
     struct kobject mapping_kobj;
@@ -40,15 +30,3 @@ void store_key(struct mapping_data* data, const char* key_str);
 int store_value(struct mapping_data* data, const char* value_str);
 void store_command(struct mapping_data* data, const char* command_str, struct map_element_data** map_elements,
                    size_t* current_elements_count);
-
-void update_element(struct mapping_data* data, struct map_element_data** map_elements, size_t* current_elements_count,
-                    struct map_element_data* (*create_element)(const char*, int));
-
-void remove_element(struct mapping_data* data, struct map_element_data** map_elements, size_t* current_elements_count,
-                    void (*destroy_element)(struct map_element_data* element_data));
-
-void get_element_value(struct mapping_data* data, struct map_element_data** map_elements,
-                       size_t current_elements_count);
-
-void reset_elements(struct mapping_data* data, struct map_element_data** map_elements, size_t* current_elements_count,
-                    void (*destroy_element)(struct map_element_data* element_data));
--- KernelModules/Mapping/source/mapping_impl.c
+++ KernelModules/Mapping/source/mapping_impl.c
@@ -8,8 +8,141 @@
 static const char* dirty_status_str = "dirty";
 static const char* synced_status_str = "synced";
 
+static const char* update_command = "update";
+static const char* remove_command = "remove";
+static const char* get_command = "get";
+static const char* reset_command = "reset";
+
+static const size_t update_command_length = 6;
+static const size_t remove_command_length = 6;
+static const size_t get_command_length = 3;
+static const size_t reset_command_length = 5;
+
 static struct map_element_data* (*create_map_element)(const char*, int) = NULL;
 static void (*destroy_map_element)(struct map_element_data* element_data) = NULL;
+
+static void update_element(struct mapping_data* data, struct map_element_data** map_elements,
+                           size_t* current_elements_count, struct map_element_data* (*create_element)(const char*, int))
+{
+    if (strlen(data->key) > 0)
+    {
+        int should_add_element = 1;
+
+        for (size_t index = 0; index < *current_elements_count; ++index)
+        {
+            if (strncmp(map_elements[index]->map_element_kobj.name, data->key, strlen(data->key)) == 0)
+            {
+                map_elements[index]->value = data->value;
+                should_add_element = 0;
+                break;
+            }
+        }
+
+        if (should_add_element)
+        {
+            if (*current_elements_count < MAX_ELEMENTS_COUNT)
+            {
+                struct map_element_data* element_data = create_element(data->key, data->value);
+
+                if (element_data)
+                {
+                    map_elements[*current_elements_count] = element_data;
+                    ++*current_elements_count;
+                }
+            }
+            else
+            {
+                pr_err("%s: cannot add element, maximum count has been reached\n", THIS_MODULE->name);
+            }
+        }
+
+        memset(data->status, '\0', MAX_STATUS_STR_LENGTH);
+        strncpy(data->status, synced_status_str, strlen(synced_status_str));
+    }
+    else
+    {
+        pr_warn("%s: cannot add element, key is empty\n", THIS_MODULE->name);
+    }
+}
+
+static void remove_element(struct mapping_data* data, struct map_element_data** map_elements,
+                           size_t* current_elements_count,
+                           void (*destroy_element)(struct map_element_data* element_data))
+{
+    int element_found = 0;
+
+    for (size_t index = 0; index < *current_elements_count; ++index)
+    {
+        if (strncmp(map_elements[index]->map_element_kobj.name, data->key, strlen(data->key)) == 0)
+        {
+            element_found = 1;
+            destroy_element(map_elements[index]);
+
+            if (*current_elements_count > 1 && index < *current_elements_count - 1)
+            {
+                map_elements[index] = map_elements[*current_elements_count - 1];
+            }
+
+            map_elements[*current_elements_count - 1] = NULL;
+            --*current_elements_count;
+            break;
+        }
+    }
+
+    if (element_found)
+    {
+        memset(data->key, '\0', MAX_KEY_STR_LENGTH);
+        data->value = 0;
+        memset(data->status, '\0', MAX_STATUS_STR_LENGTH);
+        strncpy(data->status, synced_status_str, strlen(synced_status_str));
+    }
+    else
+    {
+        pr_err("%s: cannot remove element, key %s has not been found\n", THIS_MODULE->name, data->key);
+    }
+}
+
+static void get_element_value(struct mapping_data* data, struct map_element_data** map_elements,
+                              size_t current_elements_count)
+{
+    int element_found = 0;
+
+    for (size_t index = 0; index < current_elements_count; ++index)
+    {
+        if (strncmp(map_elements[index]->map_element_kobj.name, data->key, strlen(data->key)) == 0)
+        {
+            element_found = 1;
+            data->value = map_elements[index]->value;
+            break;
+        }
+    }
+
+    if (!element_found)
+    {
+        pr_warn("%s: key %s has not been found, setting default value\n", THIS_MODULE->name, data->key);
+        data->value = 0;
+    }
+
+    memset(data->status, '\0', MAX_STATUS_STR_LENGTH);
+    strncpy(data->status, synced_status_str, strlen(synced_status_str));
+}
+
+static void reset_elements(struct mapping_data* data, struct map_element_data** map_elements,
+                           size_t* current_elements_count,
+                           void (*destroy_element)(struct map_element_data* element_data))
+{
+    for (size_t index = 0; index < *current_elements_count; ++index)
+    {
+        destroy_element(map_elements[index]);
+        map_elements[index] = NULL;
+    }
+
+    *current_elements_count = 0;
+    memset(data->key, '\0', MAX_KEY_STR_LENGTH);
+    data->value = 0;
+    memset(data->status, '\0', MAX_STATUS_STR_LENGTH);
+    strncpy(data->status, synced_status_str, strlen(synced_status_str));
+}
 
 int init_data(struct mapping_data* data, struct map_element_data* (*create_element)(const char*, int),
               void (*destroy_element)(struct map_element_data* element_data))
@@ -90,124 +223,4 @@ void store_command(struct mapping_data* data, const char* command_str, struct ma
     {
         pr_warn("%s: invalid command: %s\n", THIS_MODULE->name, data->command);
     }
-}
-
-void update_element(struct mapping_data* data, struct map_element_data** map_elements, size_t* current_elements_count,
-                    struct map_element_data* (*create_element)(const char*, int))
-{
-    if (strlen(data->key) > 0)
-    {
-        int should_add_element = 1;
-
-        for (size_t index = 0; index < *current_elements_count; ++index)
-        {
-            if (strncmp(map_elements[index]->map_element_kobj.name, data->key, strlen(data->key)) == 0)
-            {
-                map_elements[index]->value = data->value;
-                should_add_element = 0;
-                break;
-            }
-        }
-
-        if (should_add_element)
-        {
-            if (*current_elements_count < MAX_ELEMENTS_COUNT)
-            {
-                struct map_element_data* element_data = create_element(data->key, data->value);
-
-                if (element_data)
-                {
-                    map_elements[*current_elements_count] = element_data;
-                    ++*current_elements_count;
-                }
-            }
-            else
-            {
-                pr_err("%s: cannot add element, maximum count has been reached\n", THIS_MODULE->name);
-            }
-        }
-
-        memset(data->status, '\0', MAX_STATUS_STR_LENGTH);
-        strncpy(data->status, synced_status_str, strlen(synced_status_str));
-    }
-    else
-    {
-        pr_warn("%s: cannot add element, key is empty\n", THIS_MODULE->name);
-    }
-}
-
-void remove_element(struct mapping_data* data, struct map_element_data** map_elements, size_t* current_elements_count,
-                    void (*destroy_element)(struct map_element_data* element_data))
-{
-    int element_found = 0;
-
-    for (size_t index = 0; index < *current_elements_count; ++index)
-    {
-        if (strncmp(map_elements[index]->map_element_kobj.name, data->key, strlen(data->key)) == 0)
-        {
-            element_found = 1;
-            destroy_element(map_elements[index]);
-
-            if (*current_elements_count > 1 && index < *current_elements_count - 1)
-            {
-                map_elements[index] = map_elements[*current_elements_count - 1];
-            }
-
-            map_elements[*current_elements_count - 1] = NULL;
-            --*current_elements_count;
-            break;
-        }
-    }
-
-    if (element_found)
-    {
-        memset(data->key, '\0', MAX_KEY_STR_LENGTH);
-        data->value = 0;
-        memset(data->status, '\0', MAX_STATUS_STR_LENGTH);
-        strncpy(data->status, synced_status_str, strlen(synced_status_str));
-    }
-    else
-    {
-        pr_err("%s: cannot remove element, key %s has not been found\n", THIS_MODULE->name, data->key);
-    }
-}
-
-void get_element_value(struct mapping_data* data, struct map_element_data** map_elements, size_t current_elements_count)
-{
-    int element_found = 0;
-
-    for (size_t index = 0; index < current_elements_count; ++index)
-    {
-        if (strncmp(map_elements[index]->map_element_kobj.name, data->key, strlen(data->key)) == 0)
-        {
-            element_found = 1;
-            data->value = map_elements[index]->value;
-            break;
-        }
-    }
-
-    if (!element_found)
-    {
-        pr_warn("%s: key %s has not been found, setting default value\n", THIS_MODULE->name, data->key);
-        data->value = 0;
-    }
-
-    memset(data->status, '\0', MAX_STATUS_STR_LENGTH);
-    strncpy(data->status, synced_status_str, strlen(synced_status_str));
-}
-
-void reset_elements(struct mapping_data* data, struct map_element_data** map_elements, size_t* current_elements_count,
-                    void (*destroy_element)(struct map_element_data* element_data))
-{
-    for (size_t index = 0; index < *current_elements_count; ++index)
-    {
-        destroy_element(map_elements[index]);
-        map_elements[index] = NULL;
-    }
-
-    *current_elements_count = 0;
-    memset(data->key, '\0', MAX_KEY_STR_LENGTH);
-    data->value = 0;
-    memset(data->status, '\0', MAX_STATUS_STR_LENGTH);
-    strncpy(data->status, synced_status_str, strlen(synced_status_str));
 }
