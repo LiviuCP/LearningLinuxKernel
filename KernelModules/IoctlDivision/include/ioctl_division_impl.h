85fbaf34b4c971341684cfcbe2032ba2c8a3c846
Liviu Popa <liviucst@gmail.com>
2026-05-12 20:51:06 +0300

    Small refactoring performed on IoctlDivision module

--- KernelModules/IoctlDivision/include/ioctl_division_impl.h
+++ KernelModules/IoctlDivision/include/ioctl_division_impl.h
@@ -1,20 +1,5 @@
 #pragma once
 
-#define SUCCESS 0
-#define SUPPORTED_MINOR_NUMBERS_COUNT 1
-
-// 9999 is an arbitrarily chosen "magic number" (in a "real" (production) system an official assignment would be
-// required; might be the major driver number)
-#define IOCTL_STORE_DIVIDED_VALUE _IOW(9999, 'a', int*)
-#define IOCTL_SHOW_DIVIDED_VALUE _IOR(9999, 'b', int*)
-#define IOCTL_STORE_DIVIDER_VALUE _IOW(9999, 'c', int*)
-#define IOCTL_SHOW_DIVIDER_VALUE _IOR(9999, 'd', int*)
-#define IOCTL_SHOW_QUOTIENT_VALUE _IOR(9999, 'e', int*)
-#define IOCTL_SHOW_REMAINDER_VALUE _IOR(9999, 'f', int*)
-#define IOCTL_DIVIDE _IOW(9999, 'g', void*)
-#define IOCTL_RESET _IOW(9999, 'h', void*)
-#define IOCTL_GET_SYNCED_STATUS _IOR(9999, 'i', bool*)
-
 long ioctl_store_divided_value(const int* divided_value);
 long ioctl_show_divided_value(int* divided_value);
 long ioctl_store_divider_value(const int* divider_value);
--- KernelModules/IoctlDivision/source/ioctl_division_main.c
+++ KernelModules/IoctlDivision/source/ioctl_division_main.c
@@ -6,6 +6,21 @@
 
 #include "ioctl_division_impl.h"
 
+#define SUCCESS 0
+#define SUPPORTED_MINOR_NUMBERS_COUNT 1
+
+// 9999 is an arbitrarily chosen "magic number" (in a "real" (production) system an official assignment would be
+// required; might be the major driver number)
+#define IOCTL_STORE_DIVIDED_VALUE _IOW(9999, 'a', int*)
+#define IOCTL_SHOW_DIVIDED_VALUE _IOR(9999, 'b', int*)
+#define IOCTL_STORE_DIVIDER_VALUE _IOW(9999, 'c', int*)
+#define IOCTL_SHOW_DIVIDER_VALUE _IOR(9999, 'd', int*)
+#define IOCTL_SHOW_QUOTIENT_VALUE _IOR(9999, 'e', int*)
+#define IOCTL_SHOW_REMAINDER_VALUE _IOR(9999, 'f', int*)
+#define IOCTL_DIVIDE _IOW(9999, 'g', void*)
+#define IOCTL_RESET _IOW(9999, 'h', void*)
+#define IOCTL_GET_SYNCED_STATUS _IOR(9999, 'i', bool*)
+
 MODULE_LICENSE("GPL");
 
 MODULE_DESCRIPTION("This module divides two integers and provides the quotient and remainder.\nAll operations are "
